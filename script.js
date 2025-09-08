3da4944a6d2707ffc38d82b0065b52b397c3636d
varunchristo31-cmyk <varunchristo31@gmail.com>
2025-09-08 15:46:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Show or hide contact details
+function showContact() {
+  let contact = document.getElementById("contact-info");
+  if (contact.style.display === "none") {
+    contact.style.display = "block";
+  } else {
+    contact.style.display = "none";
+  }
+}
